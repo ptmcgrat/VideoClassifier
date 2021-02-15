c267f3614a49b037b430f2f847215b2311cefd99
Patrick McGrath <ptmcgrat@gmail.com>
2021-02-15 11:23:23 -0500

    First commit

--- Utils/CichlidActionRecognition.py
+++ Utils/CichlidActionRecognition.py
@@ -25,7 +25,7 @@ class ML_model():
 
     def createModel(self):
         print('Creating Model')
-
+        self.model = resnet18(num_classes=self.n_classes, shortcut_type='B', sample_size=self.xy_crop, sample_duration=self.t_size)
         self.model = self.model.cuda()
         self.model = nn.DataParallel(self.model, device_ids=None)
         self.parameters = self.model.parameters()
@@ -86,9 +86,7 @@ class ML_model():
         optimizer = optim.SGD(self.parameters, lr=learning_rate, momentum=momentum, dampening=dampening, weight_decay=weight_decay, nesterov=nesterov)
         scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=lr_patience)
 
-        if trainedModel is None:
-            self.model = resnet18(num_classes=self.n_classes, shortcut_type='B', sample_size=self.xy_crop, sample_duration=self.t_size)
-        else:
+        if trainedModel is not None:
             checkpoint = torch.load(opt.Trained_model)
             begin_epoch = checkpoint['epoch']
             model.load_state_dict(checkpoint['state_dict'])
